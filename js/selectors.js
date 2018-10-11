37fd8704ba33de643c8353691a8deaba69dbc245
Hamza Hedfi <h.hedfi@outlook.com>
2018-10-11 16:01:02 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,19 @@
-// declare your functions here...
+function paragrapheSelector() {
+  return $("p");
+}
+
+function lastImageSelector(){
+  return $("div img:last-child")
+}
+
+function ninjaBabySelector() {
+  return $("#baby-ninja");
+}
+
+function divSelector() {
+  $(".pics");
+}
+
+function firstListItem(){
+  return $("#pic-list ul:first-child");
+}
